cee45743b85a564b9cde66975d72e78080d8a969
Sunidhi <81474848+sunidhii09@users.noreply.github.com>
2024-05-27 16:10:03 +0530

    Update prediction.py

--- Models/prediction.py
+++ Models/prediction.py
@@ -6,11 +6,8 @@ import numpy as np
 model = keras.models.load_model('my_model.keras')
 
 def preprocess_audio():
-    # Load the audio file
     audio_data, sr = librosa.load(audio_file, sr=None)
-    # Compute the mel spectrogram 
     spectrogram = librosa.feature.melspectrogram(y=audio_data, sr=sr)
-    # Resize the spectrogram to match the input shape of the model
     resized_spectrogram = np.resize(spectrogram, (model.input_shape[1], 1))
     return np.expand_dims(resized_spectrogram, axis=0)
 
