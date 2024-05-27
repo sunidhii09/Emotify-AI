44e98bc63fffb425a750d9f1f847351fc5a1e4ea
Sunidhi <81474848+sunidhii09@users.noreply.github.com>
2024-05-27 16:09:42 +0530

    Update prediction.py

--- Models/prediction.py
+++ Models/prediction.py
@@ -3,10 +3,8 @@ from tensorflow import keras
 import librosa
 import numpy as np
 
-# Load the trained model
 model = keras.models.load_model('my_model.keras')
 
-# Preprocess the audio
 def preprocess_audio():
     # Load the audio file
     audio_data, sr = librosa.load(audio_file, sr=None)
@@ -16,13 +14,11 @@ def preprocess_audio():
     resized_spectrogram = np.resize(spectrogram, (model.input_shape[1], 1))
     return np.expand_dims(resized_spectrogram, axis=0)
 
-# Make predictions
-audio_file = 'Recording2.wav'
+audio_file = 'add your path'
 def make_predictions():
     preprocessed_audio = preprocess_audio()
     predictions = model.predict(preprocessed_audio)
     predicted_class = np.argmax(predictions)
     return predicted_class
-
-# Test on a .wav file
+    
 make_predictions()
